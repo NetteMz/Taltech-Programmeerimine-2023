e634c5037ed238ad9f3a354ed6538f7f5adf5ff8
irispajus <124361387+irispajus@users.noreply.github.com>
2023-02-21 20:34:38 +0200

    Add files via upload

---
+++ FirstExercise.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	int findContentChildren(vector<int>& g, vector<int>& s) {
+		sort(g.begin(), g.end());
+		sort(s.begin(), g.end());
+
+		int contentChildren = 0;
+		int i = 0;
+		int j = 0;
+
+		while (i < g.size() && j < s.size()) {
+			if (s[j]) >= g[i]{
+				contentChildren++,
+				i++;
+			}
+			j++;
+		}
+
+		return contentChildren;
+
+	}
+};
