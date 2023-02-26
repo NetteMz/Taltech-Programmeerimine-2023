965bed7630ee1f8c432fc0d0f3937320a960b7c5
irispajus <124361387+irispajus@users.noreply.github.com>
2023-02-26 20:11:49 +0200

    Update Kodutöö_1_yl_1.cpp
    if (s[j]) = g[i] { parandasin if (s[j] == g[i]) { sest muidu sain errori ning nihutasin contentChildren asukohta. Uuendasin koodi uuesti, sest osa asju oli valel real.

--- Kodutöö_1_yl_1.cpp
+++ Kodutöö_1_yl_1.cpp
@@ -1,27 +1,25 @@
 class Solution {
-	//sorteerib isufaktorid ja lapsed	
-public:
-	int findContentChildren(vector<int>& g, vector<int>& s) {
-		sort(g.begin(), g.end());
-		sort(s.begin(), g.end());
-		// peab meeles kui palju igale lapsele antakse vastavalt selle kui suur tema isu on
-		int contentChildren = 0;
-		int i = 0;
-		int j = 0;
-		/*
-		annab lapsele küpsise/küpsised vastavalt isule ja liigub siis järgmise lapse juurde 
-		kuni küpsised on otsas ja saame teada paljd lapsed saavad nendest küpsistest isu täis
+public: //sorteerib isufaktorid ja lapsed
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end()); 
+        // peab meeles kui palju igale lapsele antakse vastavalt selle kui suur tema isu on
+        int contentChildren = 0;
+        int i = 0;
+        int j = 0;
+        /*
+		annab lapsele kÃ¼psise/kÃ¼psised vastavalt isule ja liigub siis jÃ¤rgmise lapse juurde 
+		kuni kÃ¼psised on otsas ja saame teada paljd lapsed saavad nendest kÃ¼psistest isu tÃ¤is
 		*/
-		while (i < g.size() && j < s.size()) {
-			if (s[j]) >= g[i]{
-				contentChildren++,
-				i++;
-			}
-			j++;
-		}
+        while (i < g.size() && j < s.size()) {
+            if (s[j] == g[i]) {
+                contentChildren++;
+                i++;
+            }
+            j++;
+        }
 
-		return contentChildren; //annab tulemuseks paljud lapsed isu täis said
-
-	}
+        return contentChildren; //annab tulemuseks paljud lapsed isu tÃ¤is said
+    }
 };
 
